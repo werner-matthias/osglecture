649cac0a2946c8e592fff3ee1246e090ce7a12a3
Matthias Werner <matthias.werner@cs.tu-chemnitz.de>
2026-06-08 10:36:32 +0200

    restructure bundle
    - modul/build.lua => minimal
    - doc directory at bundle level

--- build.lua
+++ build.lua
@@ -1,28 +1,130 @@
 bundle   = "osglecture"
 ctanpkg  = bundle
 
---[ modules = {
-  --{ name = "osglecture" },
-  --{ name = "langselect" },
-  --{ name = "osgterminal" },
-  -- { name = "osglecturebib" },
-  --{ name = "osgcombine" },
-  --{ name = "osglisting" },
--- }
---]
+modules = {
+   "langselect",
+   "osgdoc",
+   -- "osglecture"
+   -- "osgterminal",
+   -- "osgref",
+   -- "osglectbib",
+   -- "osglisting"
+ }
 
 textfiles = { "README.md", "CHANGES.md", "LICENSE" }
 
--- Script-Auslieferung (landet in TEXMF/scripts/osglecture/)
-scriptfiles = {
-  "scripts/ollm",
-  "scripts/ollm.bat",   -- optional; für Windows bequem
+unpackfiles = { "*.dtx" }
+
+stdengine    = "luatex"
+checkengines = { "luatex" }
+
+-- Dokumentation   
+typesetexe = "lualatex"
+typesetopts = "-interaction=nonstopmode -shell-escape"
+maxruns    = 3
+
+cleanfiles={
+    "*-cnltx*", -- artefacts from cnltx tools
+    "*.toc",
+    "*.aux",
+    "*.log",
+    "*.idx",
+    "*.ilg",
+    "*.ind"
 }
 
--- Globale Defaults (pro Modul überschreibbar)
-sourcefiles  = { "*.dtx", "doc/*.tex" }
-typesetfiles = { "doc/*-doc.tex", "*.dtx"}
+--[[ 
+The documentation is in two languages, English and German.
+I.e., each .dtx file has to be compiled twice.
+We use langselect and get the target language from jobname.
+Thus, we need a special typeset function.
+--]]
 
+function typeset(file, dir, cmd)
+   dir = dir or "."
+   local jobnames
+   local ext = file:match("%.([^.]*)$")
+   if ext == 'dtx' then 
+      jobnames = {module.."-en", module.."-de"}
+   else
+      local jobname = file:match("(%).[^.]*$")
+      jobnames = {jobname}
+   end
+   for _, job in ipairs(jobnames) do
+      local errorlevel
+      
+      for i = 1, typesetruns do
+	 errorlevel = tex(file, dir, cmd .. " -jobname=" .. job)
+	 if errorlevel ~= 0 then return errorlevel end
+	 
+	 if i == 1 then
+	    makeindex(job, dir, ".idx", ".ind", ".ilg", indexstyle)
+	 end
+	 
+	 if i > 1 and not rerun_needed(job, dir) then
+	    break
+	 end
+      end
+      --[[ Actually, doc() is responsible to save the results.
+	   However, it can't cope with changed file stems.
+      --]]
+      cp(job..".pdf", typesetdir, docfiledir)
+   end
+   
+  return 0
+end
+
+function rerun_needed(job, dir)
+  local log = io.open(dir .. "/" .. job .. ".log", "r")
+  if not log then return false end
+  local s = log:read("*all")
+  log:close()
+
+  return
+    s:find("Rerun to get cross%-references right") or
+    s:find("Label%(s%) may have changed") or
+    s:find("There were undefined references") or
+    s:find("Rerun LaTeX")
+end
+
+--[[
+  I want a two-level clean:
+  - 'l3build clean' keeps the final build files (pdf)
+  - 'l3build cleanall' cleans everything
+--]]
+stdclean = target_list.clean.func
+
+function cleanlite()
+  for _, pattern in ipairs(cleanfiles or {}) do
+    rm(typesetdir, pattern)
+  end
+  return 0
+end
+
+target_list.clean.func = cleanlite
+
+target_list.cleanall = {
+  desc = "Cleans all generated files",
+  func = stdclean,
+}
+
+
+-- Script-Auslieferung (landet in TEXMF/scripts/osglecture/)
+-- scriptfiles = {
+--  "scripts/ollm",
+--  "scripts/ollm.bat",   -- optional; für Windows bequem
+--}
+
+docfiles     = {
+  "*.dtx",
+  "*.pdf",
+  "README.md"
+}
+
+sourcefiles  = { "*.dtx"}
+typesetfiles = { "*.dtx"}
+
+--[[
 -- Tests: Standard-Lauf (Unit je Modul) + Integrations-Lauf (siehe config-integ.lua)
 -- checkconfigs = { "build", "config-integ" }
 checkconfigs = { "build"}
@@ -37,26 +139,16 @@ checkdeps = {
   --"./osglisting",
 }
 
-unpackfiles = {
-   "*.dtx"
-}
-
--- Engines global (Integration nutzt lualatex + pdflatex; Unit-Tests pro Modul können enger sein)
-stdengine    = "lualatex"
-checkengines = { "luatex" }
-
--- Doku
-typesetexe = "lualatex"
-maxruns    = 3
 
 -- Tagging (optional)
 tagfiles = {
-  "osglecture/osglecture.dtx",
-  "multibabel/multibabel.dtx",
-  "osgterminal/osgterminal.dtx",
-  "osgbib/osgbib.dtx",
-  "osgcombine/osgcombine.dtx",
-  "osglisting/osglisting.dtx",
+  -- "osglecture/osglecture.dtx",
+  -- "multibabel/multibabel.dtx",
+  -- "osgterminal/osgterminal.dtx",
+  -- "osgbib/osgbib.dtx",
+  -- "osgcombine/osgcombine.dtx",
+  -- "osglisting/osglisting.dtx",
   "scripts/ollm",
 }
 tagfmt = "v%Y-%m-%d"
+--]]
--- langselect/build.lua
+++ langselect/build.lua
@@ -1,56 +1,10 @@
 bundle = "osglecture"
 module = "langselect"
 
-mainindir=".."
+maindir="../"
+docfiledir = "../doc"
 
-unpackfiles = { "langselect.dtx" }
+installfiles = { "langselect.sty" }
+typesetsearch = true
 
-stdengine    = "luatex"
-checkengines = { "luatex" }
-
-sourcefiles  = { "langselect.dtx" }
-
-docfiles     = {
-  "langselect.dtx",
-  "langselect.pdf",
-  "langselect-de.pdf",
-  "README.txt"
-}
-
-jobnames = { "langselect", "langselect-de" }
-
-typesetfiles = { "langselect.dtx" } 
-
-typesetexe = "lualatex"
-typesetopts = "-interaction=nonstopmode -shell-escape"
-
-cleanfiles={
-    "*-cnltx*",
-    "*.toc",
-    "*.aux",
-    "*.log",
-    "*.fdb_latexmk",
-    "*.fls",
-    "*.idx",
-    "*.ilg",
-    "*.ind"
-}
-
---  
-function typeset(name, engine, _)
-   print(">>> typeset() called with engine: " .. engine)
-  for _, job in ipairs(jobnames) do
-    local cmd = string.format("%s %s -jobname=%s langselect.dtx", typesetexe, typesetopts, job)
-    print("Running: " .. cmd)
-    for _ = 1,typesetruns do 
-       local result = os.execute(cmd)
-    end
-    if result ~= 0 then
-      return result
-    end
-  end
-  return 0
-end
-
-
--- dofile("../build.lua")
+dofile("../build.lua")
